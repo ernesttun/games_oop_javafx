aaf54c0b2b5bfface6342f065eb4eb6fa4ab5378
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-07-30 14:55:21 +0300

    3. Каркас шахматной доски [#792  [#792 #431609]]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -3,7 +3,9 @@ package ru.job4j.chess;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
 import ru.job4j.chess.firuges.black.BishopBlack;
+import ru.job4j.chess.firuges.black.PawnBlack;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -16,7 +18,7 @@ public class LogicTest {
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
-            logic.move(Cell.C1, Cell.H6);
+            logic.move(null, Cell.H6);
         });
         assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
     }
@@ -26,10 +28,26 @@ public class LogicTest {
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         BishopBlack bishopBlack = new BishopBlack(Cell.C8);
+        logic.add(bishopBlack);
         ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class,
                 () -> {
                     logic.move(bishopBlack.position(), Cell.C6);
                 });
-        assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from %s to %s", bishopBlack.position(), Cell.C7);
+        assertThat(exception.getMessage()).isEqualTo("Could not way by diagonal from %s to %s", bishopBlack.position(), Cell.C6);
+    }
+
+    @Test
+    public void whenThenOccupiedCellException()
+    throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        BishopBlack black = new BishopBlack(Cell.C1);
+        PawnBlack pawnBlack = new PawnBlack(Cell.E3);
+        logic.add(black);
+        logic.add(pawnBlack);
+        OccupiedCellException exception = assertThrows(OccupiedCellException.class,
+                () -> {
+                    logic.move(black.position(), Cell.G5);
+                });
+        assertThat(exception.getMessage()).isEqualTo("the cell is occupied");
     }
 }
